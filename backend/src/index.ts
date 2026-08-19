9506eaa5116c6cbf0de1a3cecfb2fa7c26a2e16c
Manish <manishrajput05122005@gmail.com>
2026-08-19 22:32:24 +0530

    fix: correct server port initialization and improve console log message

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,8 +4,8 @@ dotenv.config({
 });
 import app from "./app.js";
 
-const PORT = process.env.PORT || 4001;
+const PORT = Number(process.env.PORT) || 4000;
 
-app.listen(PORT, () => {
-  console.log(`server running successfuly on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
