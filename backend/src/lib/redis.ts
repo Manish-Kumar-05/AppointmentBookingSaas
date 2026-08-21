99d061f1bf8299311b6c51580a9b4423deb54185
Manish <manishrajput05122005@gmail.com>
2026-08-21 19:46:54 +0530

    fix: update redis connection to use environment variable for configuration

--- backend/src/lib/redis.ts
+++ backend/src/lib/redis.ts
@@ -6,8 +6,18 @@
 //   maxRetriesPerRequest: null,
 // });
 
+// export const redisConnection = {
+//   host: "127.0.0.1",
+//   port: 6379,
+//   maxRetriesPerRequest: null,
+// };
+
+import "dotenv/config";
+
+const redisUrl = new URL(process.env.REDIS_URL!);
+
 export const redisConnection = {
-  host: "127.0.0.1",
-  port: 6379,
+  host: redisUrl.hostname,
+  port: Number(redisUrl.port),
   maxRetriesPerRequest: null,
 };
