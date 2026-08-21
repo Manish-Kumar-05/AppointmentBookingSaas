86294d8438bb2155fb9a17f1b6185428b9a11851
Manish <manishrajput05122005@gmail.com>
2026-08-21 22:17:27 +0530

    fix production api url

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,5 +1,36 @@
+// import axios from "axios";
+// import { tokenService } from "./auth-token";
+
+// export const api = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL,
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = tokenService.getToken();
+
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return config;
+// });
+
+// api.interceptors.response.use(
+//   (res) => res,
+//   (error) => {
+//     // Just clear token
+//     if (error.response?.status === 401) {
+//       tokenService.clearToken();
+//     }
+
+//     return Promise.reject(error);
+//   },
+// );
+
 import axios from "axios";
 import { tokenService } from "./auth-token";
+
+console.log("BACKEND URL:", process.env.NEXT_PUBLIC_BACKEND_API_URL);
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL,
@@ -7,6 +38,8 @@ export const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = tokenService.getToken();
+
+  console.log("REQUEST URL:", `${config.baseURL}${config.url}`);
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
@@ -18,7 +51,8 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (res) => res,
   (error) => {
-    // Just clear token
+    console.error("AXIOS ERROR:", error);
+
     if (error.response?.status === 401) {
       tokenService.clearToken();
     }
